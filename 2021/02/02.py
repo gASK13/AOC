08ff93b3807a398bb02abfc4657a910db50f5a25
gASK13 <gask13@gmail.com>
2021-12-02 08:40:36 +0100

    2021/02: I feel almost ashamed for such a simple solution, but ... meh.

---
+++ 2021/02/02.py
@@ -0,0 +1,19 @@
+with open('input.txt','r') as f:
+    lines = f.readlines()
+    v2 = 0
+    v1 = 0
+    h = 0
+    aim = 0
+    for line in lines:
+        num = int(line.split(' ')[1])
+        if line.startswith('up'):
+            aim -= num
+            v1 -= num
+        if line.startswith('down'):
+            aim += num
+            v1 += num
+        if line.startswith('forward'):
+            h += num
+            v2 += aim*num
+print(h*v1)
+print(h*v2)
