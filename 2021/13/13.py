04b21850829d34c411eff420cc18ad68c2ae1491
gASK13 <gask13@gmail.com>
2021-12-13 12:32:09 +0100

    2021/13: Code printing from matrix. Should have seen this coming.
    PS: I love and hate the one liner in print at the same time. This is pure python.

---
+++ 2021/13/13.py
@@ -0,0 +1,45 @@
+import common
+
+
+class Matrix:
+    def __init__(self, lines):
+        self.matrix = set()
+        for line in lines:
+            x = int(line.split(',')[0])
+            y = int(line.split(',')[1])
+            self.matrix.add((x, y))
+
+    def fold(self, line):
+        val = int(line.split('=')[1])
+        if line.split('=')[0] == 'fold along y':
+            flip = [(x, val - (y - val)) for x, y in self.matrix if y > val]
+            self.matrix = set([(x, y) for x, y in self.matrix if y < val] + flip)
+        else:
+            flip = [(val - (x - val), y) for x, y in self.matrix if x > val]
+            self.matrix = set([(x, y) for x, y in self.matrix if x < val] + flip)
+
+    def dot_count(self):
+        return len(self.matrix)
+
+    def print(self):
+        for line in [''.join(['#' if (i, j) in self.matrix else ' ' for i in range(1 + max([x for x, y in self.matrix]))])
+                     for j in range(1 + max([y for x, y in self.matrix]))]:
+            print(line)
+
+
+def run(lines):
+    data = lines[:lines.index('')]
+    instructions = lines[lines.index('') + 1:]
+    m = Matrix(data)
+    for line in instructions:
+        m.fold(line)
+        if line == instructions[0]:
+            print(f'First fold is {m.dot_count()}')
+    return m
+
+
+run(common.Loader.load_lines("test.txt"))
+m = run(common.Loader.load_lines())
+m.print()
+
+# 18->17->16
