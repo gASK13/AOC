49b9917a7abcf848e7452c8ac83050b66b907d9a
gASK13 <gask13@gmail.com>
2021-12-22 19:13:22 +0100

    2021/22: Phew. Did part one in anticipation of part two but made an error in intersection algorithm that was not in test data. Bummer.

--- 2021/22/22.py
+++ 2021/22/22.py
@@ -2,40 +2,103 @@ import common
 
 
 class Cube:
-    def __init__(self, line):
-        self.on = 1 if line.split(' ')[0] == 'on' else 0
-
-        self.__minx = int(line.split(',')[0].split('=')[1].split('..')[0])
-        self.__maxx = int(line.split(',')[0].split('=')[1].split('..')[1])
-
-        self.__miny = int(line.split(',')[1].split('=')[1].split('..')[0])
-        self.__maxy = int(line.split(',')[1].split('=')[1].split('..')[1])
-
-        self.__minz = int(line.split(',')[2].split('=')[1].split('..')[0])
-        self.__maxz = int(line.split(',')[2].split('=')[1].split('..')[1])
+    def __init__(self, line=None, axes=None, on=True):
+        if line is None:
+            self.on = on
+            self.__axes = axes
+        else:
+            self.on = True if line.split(' ')[0] == 'on' else False
+            self.__axes = [[int(x) for x in line.split(',')[0].split('=')[1].split('..')],
+                           [int(y) for y in line.split(',')[1].split('=')[1].split('..')],
+                           [int(z) for z in line.split(',')[2].split('=')[1].split('..')]]
 
     def __intersects(self, other):
-        if (other.__minx <= self.__minx <= other.__maxx
-            or other.__minx <= self.__maxx <= other.__maxx) and \
-            (other.__miny <= self.__miny <= other.__maxy
-             or other.__miny <= self.__maxy <= other.__maxy) and \
-            (other.__minz <= self.__minz <= other.__maxz
-             or other.__minz <= self.__maxz <= other.__maxz):
-            return True
-        return False
+        for i in range(3):
+            if self.__axes[i][0] > other.__axes[i][1] or self.__axes[i][1] < other.__axes[i][0]:
+                return False
+        return True
 
-    def intersects(self, other):
-        return self.__intersects(other) or other.__intersects(self)
+    def __str__(self):
+        return '~'.join(['..'.join([str(n) for n in a]) for a in self.__axes])
 
-    def within_bounds(self, nr):
-        return nr >= self.__minx >= -nr
+    def __repr__(self):
+        return self.__str__()
+
+    def volume_limit(self, nr):
+        test_cube = Cube(axes=[[-nr, nr], [-nr, nr], [-nr, nr]])
+        if not self.__intersects(test_cube):
+            return 0
+        vol = 1
+        for a in self.__axes:
+            vol *= min(a[1], nr) - max(a[0], -nr) + 1
+        return vol
+
+    def break_by(self, other):
+        if self.__intersects(other):
+            breaks = self.get_breaks(other)
+            broken_cubes = []
+            for bx in breaks[0]:
+                for by in breaks[1]:
+                    for bz in breaks[2]:
+                        if any([bx[2], by[2], bz[2]]):
+                            broken_cubes.append(Cube(axes=[[bx[0], bx[1]], [by[0], by[1]], [bz[0], bz[1]]]))
+            # break by other
+            # each axis can be -> whole within (single OFF)
+            # before / after / around -> whole within / outside
+            return broken_cubes
+        return [self]
+
+    def get_breaks(self, other):
+        breaks = [[], [], []]
+        for i in range(3):
+            if other.__axes[i][0] > self.__axes[i][0]:
+                # the begining is "plus"
+                if other.__axes[i][1] < self.__axes[i][1]:
+                    # the ending is "plus" and there is "minus"
+                    breaks[i].append([self.__axes[i][0], other.__axes[i][0] - 1, True])
+                    breaks[i].append([other.__axes[i][0], other.__axes[i][1], False])
+                    breaks[i].append([other.__axes[i][1] + 1, self.__axes[i][1], True])
+                else:
+                    # the other half is minus
+                    breaks[i].append([self.__axes[i][0], other.__axes[i][0] - 1, True])
+                    breaks[i].append([other.__axes[i][0], self.__axes[i][1], False])
+            else:
+                # the begining element is "minus"
+                if other.__axes[i][1] < self.__axes[i][1]:
+                    # the ending is "plus"
+                    breaks[i].append([self.__axes[i][0], other.__axes[i][1], False])
+                    breaks[i].append([other.__axes[i][1] + 1, self.__axes[i][1], True])
+                else:
+                    # the whole part is minus
+                    breaks[i].append([self.__axes[i][0], self.__axes[i][1], False])
+        return breaks
 
 
-cubes = common.Loader.transform_lines(Cube, 'test.txt')
-cubes = [c for c in cubes if c.within_bounds(50)]
-on_cubes =
+def calculate_volume(file=None, limit=50):
+    cubes_filter = common.Loader.transform_lines(Cube, file)
+    on_cubes = []
+    for c in cubes_filter:
+        # try to intersect with each cube in ON
+        new_on_cubes = [c] if c.on else []
+        for oc in on_cubes:
+            new_on_cubes += oc.break_by(c)
+        on_cubes = new_on_cubes
+    return sum([v.volume_limit(limit) for v in on_cubes])
 
-for c in cubes:
-    for c2 in cubes:
-        if c.intersects(c2):
-            print("#INTERSECT!")
+
+# TEST PART ONE
+print(calculate_volume('test_simple.txt'))
+assert calculate_volume('test_simple.txt') == 39
+print(calculate_volume('test.txt'))
+assert calculate_volume('test.txt') == 590784
+
+# PART TWO
+print(calculate_volume('test_large.txt', limit=1000000))
+assert calculate_volume('test_large.txt', limit=1000000) == 2758514936282235
+
+# REAL!!!
+print(calculate_volume())
+print(f'REAL = {calculate_volume()}')
+print(f'REAL PT 2= {calculate_volume(limit=1000000)}')
+
+# REAL IS 545118 (naive)
