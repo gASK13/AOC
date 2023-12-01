01a68159b0bf46f061f71c4b8aa078469bcaf6fa
gASK13 <gask13@gmail.com>
2023-12-01 10:19:50 +0100

    2023/01: Reformat code to be nicer.

--- 2023/01/01.py
+++ 2023/01/01.py
@@ -2,55 +2,60 @@ import common
 
 number_texts = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
-def compute_number(line, text = False):
-    if text:
-        return find_first_number_in_line_text(line) * 10 + find_last_number_in_line_text(line)
-    else:
-        return find_first_number_in_line(line) * 10 + find_last_number_in_line(line)
 
-def find_first_number_in_line(line):
-    for i in range(len(line)):
-        if line[i].isdigit():
-            return int(line[i])
+def compute_number(input_line, text=False):
+    if text:
+        return find_first_number_in_line_text(input_line) * 10 + find_last_number_in_line_text(input_line)
+    else:
+        return find_first_number_in_line(input_line) * 10 + find_last_number_in_line(input_line)
+
+
+def find_first_number_in_line(input_line):
+    for i in range(len(input_line)):
+        if input_line[i].isdigit():
+            return int(input_line[i])
     return 0
 
-def find_last_number_in_line(line):
-    return find_first_number_in_line(line[::-1])
+
+def find_last_number_in_line(input_line):
+    return find_first_number_in_line(input_line[::-1])
 
 
-def find_first_number_in_line_text(line):
-    for i in range(len(line)):
-        if line[i].isdigit():
-            return int(line[i])
+def find_first_number_in_line_text(input_line):
+    for i in range(len(input_line)):
+        if input_line[i].isdigit():
+            return int(input_line[i])
         for (j, number) in enumerate(number_texts):
-            if line[i:i+len(number)] == number:
+            if input_line[i:i + len(number)] == number:
                 return j
     return 0
 
-def find_last_number_in_line_text(line):
-    for i in range(len(line)):
-        if line[-i-1].isdigit():
-            return int(line[-i-1])
+
+def find_last_number_in_line_text(input_line):
+    for i in range(len(input_line)):
+        if input_line[-i - 1].isdigit():
+            return int(input_line[-i - 1])
         for (j, number) in enumerate(number_texts):
             if -i == 0:
-                if line[-i-len(number):] == number:
+                if input_line[-i - len(number):] == number:
                     return j
-            elif line[-i-len(number):-i] == number:
+            elif input_line[-i - len(number):-i] == number:
                 return j
     return 0
 
+
 test_data = {
-'1abc2' : (12, 12),
-'pqr3stu8vwx' : (38, 38),
-'a1b2c3d4e5f' : (15, 15),
-'treb7uchet' : (77, 77),
-'two1nine': (11, 29),
-'eightwothree': (0, 83),
-'abcone2threexyz' : (22, 13),
-'xtwone3four' : (33, 24),
-'4nineeightseven2' : (42,42),
-'zoneight234': (24, 14),
-'7pqrstsixteen': (77, 76)
+    '1abc2': (12, 12),
+    'pqr3stu8vwx': (38, 38),
+    'a1b2c3d4e5f': (15, 15),
+    'treb7uchet': (77, 77),
+    'two1nine': (11, 29),
+    'eightwothree': (0, 83),
+    'abcone2threexyz': (22, 13),
+    'xtwone3four': (33, 24),
+    '4nineeightseven2': (42, 42),
+    'zoneight234': (24, 14),
+    '7pqrstsixteen': (77, 76)
 }
 
 for line, (res, res_text) in test_data.items():
