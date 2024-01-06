c1c0631ab55b4bcfbb4021e1df28a12d18d89a20
gASK13 <gask13@gmail.com>
2024-01-06 11:59:23 +0100

    2015/20: Second part turned to be the same to solve, yay!

--- 2015/20/20.py
+++ 2015/20/20.py
@@ -4,41 +4,44 @@ import plotly.graph_objects as go
 
 
 def compute_house(idx):
-    divisors = []
+    _sum = 0
     for i in range(1, idx + 1):
         if idx % i == 0:
-            divisors.append(i)
-    return divisors
+            _sum += i*10
+    return _sum
+
+
+def compute_house_second(idx):
+    _sum = 0
+    for i in range(1, idx + 1):
+        if idx % i == 0 and idx / i <= 50:
+            _sum += i*11
+    return _sum
 
 
 def graph_divisors(start, count):
-    data = [compute_house(x) for x in range(start, start + count)]
+    data = [compute_house_second(x) for x in range(start, start + count)]
     # now draw it in plotly
-    fig = go.Figure(data=[go.Scatter(x=list(range(start, start + count)), y=[sum(x) for x in data])])
+    fig = go.Figure(data=[go.Scatter(x=list(range(start, start + count)), y=[x for x in data])])
     fig.show()
 
 
 def find_house(presents, start=1, step=1):
     for i in range(start, presents // 10, step):
-        if sum(compute_house(i)) * 10 >= presents:
+        if compute_house(i) >= presents:
             return i
     raise Exception('Not found :(')
 
 
-assert sum(compute_house(1)) * 10 == 10
-assert sum(compute_house(2)) * 10 == 30
-assert sum(compute_house(3)) * 10 == 40
-assert sum(compute_house(4)) * 10 == 70
-assert sum(compute_house(5)) * 10 == 60
-assert sum(compute_house(6)) * 10 == 120
-assert sum(compute_house(10)) * 10 == 180
-assert sum(compute_house(20)) * 10 == 420
+def find_house_second(presents, start=1, step=1):
+    for i in range(start, presents // 10, step):
+        if compute_house_second(i) >= presents:
+            return i
+    raise Exception('Not found :(')
 
 assert find_house(420) == 20
 
-# Used to find "local maxima" manually
-#graph_divisors(10000, 20000)
-
+# Used PLOTLY to find "local maxima" manually
 # The biggest step is by "2520" (giving quite nice peaks after 10k)
 # then by 60 (giving much smaller subpeaks)
 # then by 1 of course (in case we get lucky)
@@ -47,3 +50,8 @@ roughest = find_house(29000000, 0, 2520)
 rough = find_house(29000000, roughest - 2520, 60)
 fine = find_house(29000000, rough - 60, 1)
 print(f'Part 1: {Back.GREEN}{Fore.BLACK}{fine}')
+
+roughest = find_house_second(29000000, 0, 2520)
+rough = find_house_second(29000000, roughest - 2520, 60)
+fine = find_house_second(29000000, rough - 60, 1)
+print(f'Part 2: {Back.GREEN}{Fore.BLACK}{fine}')
