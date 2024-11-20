6ab9fdaa35d4215f1cdeb60e81a3c405d4f511a9
gASK13 <gask13@gmail.com>
2024-11-20 11:11:32 +0100

    2015/24: It is SO satisfying to write first part in such a way that second part is done in 5 minutes.

--- 2015/24/24.py
+++ 2015/24/24.py
@@ -1,5 +1,6 @@
 import common
 from colorama import Fore, Back, Style
+import tqdm
 
 def product(array):
     result = 1
@@ -7,9 +8,9 @@ def product(array):
         result *= item
     return result
 
-def find_all_groups(weights):
-    _groupwt = sum(weights) // 3
-    assert sum(weights) == (_groupwt) * 3
+def find_all_groups(weights, parts=3):
+    _groupwt = sum(weights) // parts
+    assert sum(weights) == (_groupwt) * parts
     return get_sums([], weights, _groupwt)
 
 def qe(group):
@@ -31,29 +32,40 @@ def get_sums(current, weights, target):
 def are_exclusive(g1, g2):
     return not set(g1) & set(g2)
 
-def part_one(weights):
+def divide(weights, parts=3):
     # order groups by "size" and "qe" to try to find the best quickest
-    groups = find_all_groups(weights)
+    groups = find_all_groups(weights, parts)
+    print(f'Found {len(groups)} groups to process...')
     best = {"qe": 1000000000, "group_size": len(weights)}
-    for i in range(len(groups)):
+    for i in tqdm.tqdm(range(len(groups))):
         if len(groups[i][0]) < best["group_size"] or (len(groups[i][0]) == best["group_size"] and groups[i][1] < best["qe"]):
             if has_valid_breakdown(groups, i):
                 best["qe"] = groups[i][1]
                 best["group_size"] = len(groups[i][0])
-                print(f"New best is {best['qe']} with {best['group_size']}")
     return best["qe"]
 
-def has_valid_breakdown(groups, i):
+def has_valid_breakdown(groups, i, parts=3):
     for j in range(len(groups)):
         if are_exclusive(groups[i][0], groups[j][0]):
+            ng = groups[i][0] + groups[j][0]
             for k in range(j, len(groups)):
-                if are_exclusive(groups[i][0] + groups[j][0], groups[k][0]):
-                    return True
+                if are_exclusive(ng, groups[k][0]):
+                    if parts == 3:
+                        return True
+                    else:
+                        nng = ng + groups[k][0]
+                        for l in range(k, len(groups)):
+                            if are_exclusive(nng, groups[l][0]):
+                                return True
     return False
 
 assert are_exclusive([1,2,3], [4,5,6])
 assert not are_exclusive([1,2,3], [4,5,1])
-assert part_one([1, 2, 3, 4, 5, 7, 8, 9, 10, 11]) == 99
+assert divide([1, 2, 3, 4, 5, 7, 8, 9, 10, 11]) == 99
 
-print(f'Part 1: {Fore.BLACK}{Back.GREEN}{part_one(common.Loader.load_lines(numeric=True))}')
+print(f'Part 1: {Fore.BLACK}{Back.GREEN}{divide(common.Loader.load_lines(numeric=True))}')
+
+assert divide([1, 2, 3, 4, 5, 7, 8, 9, 10, 11], 4) == 44
+
+print(f'Part 2: {Fore.BLACK}{Back.GREEN}{divide(common.Loader.load_lines(numeric=True), 4)}')
 
