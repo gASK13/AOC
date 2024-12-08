7cdbc4793371877ac4d597288ba32ec3f33c3437
gASK13 <gask13@gmail.com>
2024-12-08 10:10:38 +0100

    2024/08: ...and the second one likewise. I feel cheated, this was an even numbered day!

--- 2024/08/08.py
+++ 2024/08/08.py
@@ -1,6 +1,11 @@
 import common
 from colorama import Fore, Back
 
+def gcd(x, y):
+    while y:
+        x, y = y, x % y
+    return x
+
 class NodeMap:
     def __init__(self, matrix):
         self.matrix = matrix
@@ -26,6 +31,40 @@ class NodeMap:
         #print('\n'.join([''.join([f'{Fore.RED}#{Fore.RESET}' if (idx, idy) in antinodes else c for idx, c in enumerate(line)]) for idy, line in enumerate(self.matrix)]))
         return len(antinodes)
 
+    def resonant_antinodes(self):
+        antinodes = set()
+        for list in self.nodes.values():
+            for i in range(len(list)):
+                for j in range(i+1, len(list)):
+                    dx = list[i][0] - list[j][0]
+                    dy = list[i][1] - list[j][1]
+                    # normalize!
+                    div = gcd(dx, dy)
+                    dx //= div
+                    dy //= div
+                    # go back ...
+                    nx = list[i][0]
+                    ny = list[i][1]
+                    while len(self.matrix[0]) > nx >= 0 and len(self.matrix) > ny >= 0:
+                        antinodes.add((nx, ny))
+                        nx += dx
+                        ny += dy
+
+                    # ... and forth
+                    nx = list[j][0]
+                    ny = list[j][1]
+                    while len(self.matrix[0]) > nx >= 0 and len(self.matrix) > ny >= 0:
+                        antinodes.add((nx, ny))
+                        nx -= dx
+                        ny -= dy
+
+        #print('\n'.join([''.join([f'{Fore.RED}#{Fore.RESET}' if (idx, idy) in antinodes else c for idx, c in enumerate(line)]) for idy, line in enumerate(self.matrix)]))
+        return len(antinodes)
+
 
 assert NodeMap(common.Loader.load_matrix("test")).count_antinodes() == 14
 print(f'Part 1: {Fore.BLACK}{Back.GREEN}{NodeMap(common.Loader.load_matrix()).count_antinodes()}{Fore.RESET}{Back.RESET}')
+
+
+assert NodeMap(common.Loader.load_matrix("test")).resonant_antinodes() == 34
+print(f'Part 2: {Fore.BLACK}{Back.GREEN}{NodeMap(common.Loader.load_matrix()).resonant_antinodes()}{Fore.RESET}{Back.RESET}')
