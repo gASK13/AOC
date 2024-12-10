7e4efb0b2058d0e586c8aa123a3c9978ccfb2a9d
gASK13 <gask13@gmail.com>
2024-12-10 08:27:38 +0100

    2024/10: And part two in the same vein and in 4 minutes. I feel even smugger now!

--- 2024/10/10.py
+++ 2024/10/10.py
@@ -12,13 +12,15 @@ def find_all_trailheads(matrix):
     return trailheads
 
 def find_summits(matrix, x, y):
-    summits = set()
+    summits = {}
     buffer = [(x, y)]
     while len(buffer) > 0:
         x, y = buffer.pop()
         val = int(matrix[y][x])
         if matrix[y][x] == '9':
-            summits.add((x, y))
+            if (x, y) not in summits:
+                summits[(x, y)] = 0
+            summits[(x, y)] += 1
         for dx, dy in [(0, -1), (1, 0), (0, 1), (-1, 0)]:
             nx = x + dx
             ny = y + dy
@@ -28,10 +30,21 @@ def find_summits(matrix, x, y):
     return summits
 
 def part_one(matrix):
-    return sum([len(s) for x,y,s in find_all_trailheads(matrix)])
+    return sum([len(s.keys()) for x,y,s in find_all_trailheads(matrix)])
+
+
+def part_two(matrix):
+    return sum([sum(s.values()) for x, y, s in find_all_trailheads(matrix)])
+
 
 for i in [1,2,3,4,36]:
     assert part_one(common.Loader.load_matrix(f"test_{i}")) == i
 
 print(f'Part 1: {Fore.BLACK}{Back.GREEN}{part_one(common.Loader.load_matrix())}{Fore.RESET}{Back.RESET}')
+
+
+for i in [3,13,81,227]:
+    assert part_two(common.Loader.load_matrix(f"test2_{i}")) == i
+
+print(f'Part 2: {Fore.BLACK}{Back.GREEN}{part_two(common.Loader.load_matrix())}{Fore.RESET}{Back.RESET}')
 
