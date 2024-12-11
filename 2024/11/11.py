0885b023e015c4563584595ec9f59880742ad6c6
gASK13 <gask13@gmail.com>
2024-12-11 14:15:58 +0100

    2024/11: It was a FIVE MINUTE task if I did DFS right from the start and did not try to be smart....

--- 2024/11/11.py
+++ 2024/11/11.py
@@ -1,40 +1,43 @@
 import common
 from colorama import Fore, Back
 
-def blink_stones(stones):
-    new_stones = []
-    for stone in stones:
-        if stone == 0:
-            new_stones.append(1)
-        elif len(str(stone)) % 2 == 0:
-            stone_text = str(stone)
-            half = len(stone_text) // 2
-            new_stones.append(int(stone_text[:half]))
-            new_stones.append(int(stone_text[half:]))
-        else:
-            new_stones.append(stone * 2024)
-    return new_stones
+_CACHE = {}
+
+def blink_stone(stone, count):
+    if (stone, count) in _CACHE:
+        return _CACHE[(stone, count)]
+
+    if count == 0:
+        return 1
+
+    if stone == 0:
+        rv = blink_stone(1, count - 1)
+    elif len(str(stone)) % 2 == 0:
+        stone_text = str(stone)
+        half = len(stone_text) // 2
+        rv = blink_stone(int(stone_text[:half]), count-1) + blink_stone(int(stone_text[half:]), count-1)
+    else:
+        rv = blink_stone(stone * 2024, count-1)
+    _CACHE[(stone, count)] = rv
+    return rv
 
 
 def blink(stones, count):
-    for i in range(count):
-        stones = blink_stones(stones)
-    return stones
+    return sum([blink_stone(s, count) for s in stones])
 
 
-assert blink_stones([0,1,10,99,999]) == [1, 2024, 1, 0, 9, 9, 2021976]
-assert blink([125,17], 1) == [253000, 1, 7]
-assert blink([125,17], 2) == [253, 0, 2024, 14168]
-assert blink([125,17], 3) == [512072, 1, 20, 24, 28676032]
-assert blink([125,17], 4) == [512, 72, 2024, 2, 0, 2, 4, 2867, 6032]
-assert blink([125,17], 5) == [1036288, 7, 2, 20, 24, 4048, 1, 4048, 8096, 28, 67, 60, 32]
-assert blink([125,17], 6) == [2097446912, 14168, 4048, 2, 0, 2, 4, 40, 48, 2024, 40, 48, 80, 96, 2, 8, 6, 7, 6, 0, 3, 2]
-
-assert len(blink([125,17], 25)) == 55312
-print(f'Part 1: {Back.GREEN}{Fore.BLACK}{len(blink(common.Loader.load_matrix(delimiter=" ",numeric=True)[0], 25))}{Back.RESET}{Fore.RESET}')
+assert blink([0,1,10,99,999], 1) == 7
+assert blink([125,17], 1) == 3
+assert blink([125,17], 2) == 4
+assert blink([125,17], 3) == 5
+assert blink([125,17], 4) == 9
+assert blink([125,17], 5) == 13
+assert blink([125,17], 6) == 22
+assert blink([125,17], 25) == 55312
+print(f'Part 1: {Back.GREEN}{Fore.BLACK}{blink(common.Loader.load_matrix(delimiter=" ",numeric=True)[0], 25)}{Back.RESET}{Fore.RESET}')
 
 # Each number is "separate" so we can apply "backtracking" of steps and depth-first search!!!
 # TBD
-print(f'Part 2: {Back.GREEN}{Fore.BLACK}{len(blink(common.Loader.load_matrix(delimiter=" ",numeric=True)[0], 75))}{Back.RESET}{Fore.RESET}')
+print(f'Part 2: {Back.GREEN}{Fore.BLACK}{blink(common.Loader.load_matrix(delimiter=" ",numeric=True)[0], 75)}{Back.RESET}{Fore.RESET}')
 
 
