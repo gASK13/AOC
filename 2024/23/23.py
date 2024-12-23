e4183bc16f3ca0e45843270de47b58e297d152e9
gASK13 <gask13@gmail.com>
2024-12-23 21:53:58 +0100

    2024/23: Ok, this was not as hard as I thought. I was expecting this to run slower and require optimization, but even the simple one worked fine.

--- 2024/23/23.py
+++ 2024/23/23.py
@@ -2,7 +2,7 @@ import common
 from colorama import Fore, Style, Back
 
 def part_one(lines):
-    return len([s for s in get_sets(lines) if s[0][0] == 't' or s[1][0] == 't' or s[2][0] == 't'])
+    return len([s for s in get_sets(lines)[0] if s[0][0] == 't' or s[1][0] == 't' or s[2][0] == 't'])
 
 def get_sets(lines):
     connections = {}
@@ -23,40 +23,33 @@ def get_sets(lines):
     # now process and find triplets with "T"
     sets = set()
     for n1 in range(len(nodes)):
-        for n2 in range(n1+1, len(nodes)):
-            for n3 in range(n2+1, len(nodes)):
-                if nodes[n2] in connections[nodes[n1]] and nodes[n3] in connections[nodes[n1]] and nodes[n2] in connections[nodes[n3]]:
-                    sets.add((nodes[n1],nodes[n2], nodes[n3]))
+        for n2 in connections[nodes[n1]]:
+            for n3 in connections[nodes[n1]]:
+                if n3 in connections[n2] and n3 != n2:
+                    sets.add(tuple(sorted([nodes[n1], n2, n3])))
 
-    return sets
+    return sets, connections, nodes
 
 
 def part_two(lines):
-    connections = {}
-    nodes = []
-    for line in lines:
-        n1, n2 = line.split('-')
-        if n1 not in connections:
-            connections[n1] = []
-        if n2 not in connections:
-            connections[n2] = []
-        connections[n1].append(n2)
-        connections[n2].append(n1)
-        if n1 not in nodes:
-            nodes.append(n1)
-        if n2 not in nodes:
-            nodes.append(n2)
+    sets, connections, nodes = get_sets(lines)
 
-    # now process and find triplets with "T"
-    sets = set()
-    for n1 in range(len(nodes)):
-        for n2 in range(n1 + 1, len(nodes)):
-            for n3 in range(n2 + 1, len(nodes)):
-                if nodes[n2] in connections[nodes[n1]] and nodes[n3] in connections[nodes[n1]] and nodes[n2] in \
-                        connections[nodes[n3]]:
-                    sets.add((nodes[n1], nodes[n2], nodes[n3]))
+    while len(sets) > 0:
+        new_sets = set()
+        for s in sets:
+            # try to add a new node to the set
+            # if possible, move to new_sets
+            for n in connections[s[0]]:
+                if n not in s:
+                    if all([n in connections[s2] for s2 in s]):
+                        new_sets.add(tuple(sorted(list(s) + [n])))
 
-    return sets
+        if len(new_sets) == 0:
+            return ','.join(sorted(list(s)))
+        sets = new_sets
+
+    # we should end with
+    raise Exception('No solution found - should not happen!')
 
 
 assert part_one(common.Loader.load_lines('test')) == 7
